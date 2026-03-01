6394a53b061cd5bdc00ce0497719523aed4fe0d9
Baeva Anna <baevaanya98@gmail.com>
2026-03-01 19:31:41 +0300

    basic iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/fickleDude/metrics.git/internal/handler"
+)
+
+func main() {
+	handler := handler.MemStorageHandler{}
+	http.HandleFunc(`/update/`, handler.UpdateHandler)
+
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
+}
