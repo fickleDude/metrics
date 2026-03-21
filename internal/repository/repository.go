f9e641346d5cde02f38416681f823fe572643caa
Baeva Anna <baevaanya98@gmail.com>
2026-03-21 23:13:26 +0300

    return not found

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -44,16 +44,7 @@ func (s *MemStorage) GetMetric(name string, mType string) *model.Metrics {
 			return m
 		}
 	}
-	var value float64
-	var delta int64
-	switch mType {
-	case "gauge":
-		return &model.Metrics{ID: name, MType: mType, Value: &value}
-	case "counter":
-		return &model.Metrics{ID: name, MType: mType, Delta: &delta}
-	default:
-		return nil
-	}
+	return nil
 }
 
 func (s *MemStorage) GetMetrics() []*model.Metrics {
