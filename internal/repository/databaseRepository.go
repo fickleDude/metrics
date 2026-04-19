0f74753a04d3564c6124dc7ec615b3219fc4a4f7
Baeva Anna <baevaanya98@gmail.com>
2026-04-19 12:49:55 +0300

    add sql retry on err

--- internal/repository/databaseRepository.go
+++ internal/repository/databaseRepository.go
@@ -2,10 +2,15 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/fickleDude/metrics.git/internal/logger"
 	model "github.com/fickleDude/metrics.git/internal/model"
+
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 
 	"github.com/fickleDude/metrics.git/migrations"
 )
@@ -32,8 +37,107 @@ func (s *MemDatabaseStorage) initDatabase() error {
 	return fmt.Errorf("migration unavailable")
 }
 
+func isRetriable(pgErr *pgconn.PgError) bool {
+	switch pgErr.Code {
+	// Класс 08 - Ошибки соединения
+	case pgerrcode.ConnectionException,
+		pgerrcode.ConnectionDoesNotExist,
+		pgerrcode.ConnectionFailure:
+		return true
+
+	// Класс 40 - Откат транзакции
+	case pgerrcode.TransactionRollback, // 40000
+		pgerrcode.SerializationFailure, // 40001
+		pgerrcode.DeadlockDetected:     // 40P01
+		return true
+
+	// Класс 57 - Ошибка оператора
+	case pgerrcode.CannotConnectNow: // 57P03
+		return true
+	}
+
+	// Можно добавить более конкретные проверки с использованием констант pgerrcode
+	switch pgErr.Code {
+	// Класс 22 - Ошибки данных
+	case pgerrcode.DataException,
+		pgerrcode.NullValueNotAllowedDataException:
+		return false
+
+	// Класс 23 - Нарушение ограничений целостности
+	case pgerrcode.IntegrityConstraintViolation,
+		pgerrcode.RestrictViolation,
+		pgerrcode.NotNullViolation,
+		pgerrcode.ForeignKeyViolation,
+		pgerrcode.UniqueViolation,
+		pgerrcode.CheckViolation:
+		return false
+
+	// Класс 42 - Синтаксические ошибки
+	case pgerrcode.SyntaxErrorOrAccessRuleViolation,
+		pgerrcode.SyntaxError,
+		pgerrcode.UndefinedColumn,
+		pgerrcode.UndefinedTable,
+		pgerrcode.UndefinedFunction:
+		return false
+	}
+
+	// По умолчанию считаем ошибку неповторяемой
+	return false
+}
+
+func (s *MemDatabaseStorage) executeWithRetry(query string, args ...any) error {
+	maxRetries := 3
+	retryDelay := []int{1, 3, 5}
+	var lastError error
+
+	for attempt := 0; attempt < maxRetries; attempt++ {
+		_, lastError = s.db.Exec(query, args...)
+		if lastError == nil {
+			return nil
+		}
+		//определяем тип ошибки
+		//конвертируем в pgconn.PgError
+		var pgErr *pgconn.PgError
+		if errors.As(lastError, &pgErr) {
+			// класс 08 - Ошибки соединения
+			if isRetriable(pgErr) {
+				time.Sleep(time.Duration(retryDelay[attempt]) * time.Second)
+				continue
+			}
+		}
+		return lastError
+	}
+	return fmt.Errorf("after %d attempts, last error: %w", maxRetries, lastError)
+}
+
+func (s *MemDatabaseStorage) queryWithRetry(query string, args ...any) (*sql.Rows, error) {
+	maxRetries := 3
+	retryDelay := []int{1, 3, 5}
+	var lastError error
+
+	for attempt := range maxRetries {
+		rows, lastError := s.db.Query(query, args...)
+		if lastError == nil {
+			return rows, nil
+		}
+		//определяем тип ошибки
+		//конвертируем в pgconn.PgError
+		var pgErr *pgconn.PgError
+		if errors.As(lastError, &pgErr) {
+			// класс 08 - Ошибки соединения
+			if isRetriable(pgErr) {
+				time.Sleep(time.Duration(retryDelay[attempt]) * time.Second)
+				continue
+			}
+		} else {
+			return nil, lastError
+		}
+	}
+	return nil, fmt.Errorf("after %d attempts, last error: %w", maxRetries, lastError)
+}
+
 func (s *MemDatabaseStorage) UpdateCount(name string, delta *int64) {
-	_, err := s.db.Exec(`INSERT INTO counter (id, delta) 
+	err := s.executeWithRetry(`INSERT INTO counter (id, delta) 
 								VALUES ($1, $2)
 								ON CONFLICT (id) DO UPDATE 
 								SET delta = counter.delta + excluded.delta`, name, delta)
@@ -44,7 +148,7 @@ func (s *MemDatabaseStorage) UpdateCount(name string, delta *int64) {
 }
 
 func (s *MemDatabaseStorage) UpdateGauge(name string, value *float64) {
-	_, err := s.db.Exec(`INSERT INTO gauge (id, value) 
+	err := s.executeWithRetry(`INSERT INTO gauge (id, value) 
 								VALUES ($1, $2)
 								ON CONFLICT (id) DO UPDATE 
 								SET value = excluded.value`, name, value)
@@ -74,8 +178,9 @@ func (s *MemDatabaseStorage) GetMetric(name string, mType string) *model.Metrics
 func (s *MemDatabaseStorage) GetMetrics() []*model.Metrics {
 	var metrics []*model.Metrics
 
-	rows, err := s.db.Query("select id, delta from counter")
+	rows, err := s.queryWithRetry("select id, delta from counter")
 	if err != nil {
+		logger.Log.Error(err.Error())
 		return nil
 	}
 	defer rows.Close()
@@ -93,8 +198,9 @@ func (s *MemDatabaseStorage) GetMetrics() []*model.Metrics {
 		return nil
 	}
 
-	rows, err = s.db.Query("select id, value from gauge")
+	rows, err = s.queryWithRetry("select id, value from gauge")
 	if err != nil {
+		logger.Log.Error(err.Error())
 		return nil
 	}
 	for rows.Next() {
